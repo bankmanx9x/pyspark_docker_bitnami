d18e8bf6f8936237358e9b851afd545d5bee1bf4
bankmanx9x <nakamoorax9x@gmail.com>
2024-10-25 17:44:33 +0700

    test pyspark with env

--- pyscr/read_write_pg_env.py
+++ pyscr/read_write_pg_env.py
@@ -1,9 +1,10 @@
-import pyspark
 from pyspark.sql import SparkSession
+from dotenv import load_dotenv
+import os
 
 spark = SparkSession\
         .builder\
-        .appName("chapter1_Read PostgreSQL")\
+        .appName("chapter1_Read PostgreSQL with env")\
         .getOrCreate()
 
 """
@@ -13,14 +14,21 @@ spark.sparkContext.setLogLevel("ERROR")
 .config('spark.jars.packages', 'org.postgresql:postgresql:42.5.4')\
 """
 
+load_dotenv()
+PGSQL_host = os.environ['PGSQL_host']
+PGSQL_port = os.environ['PGSQL_port']
+PGSQL_db = os.environ['PGSQL_db']
+PGSQL_user = os.environ['PGSQL_user']
+PGSQL_password = os.environ['PGSQL_password']
+
 postgredf = spark\
             .read\
             .format("jdbc") \
-            .option("url", "jdbc:postgresql://172.25.0.4:5432/postgres") \
+            .option("url", f"jdbc:postgresql://{PGSQL_host}:{PGSQL_port}/{PGSQL_db}") \
             .option("driver", "org.postgresql.Driver") \
             .option("dbtable", "public.empl") \
-            .option("user", "postgres") \
-            .option("password", "postgres")\
+            .option("user", PGSQL_user) \
+            .option("password", PGSQL_password)\
             .load()
 
 print(postgredf.show())
