a0536b1c8908257127ec3f172e20ecdbba6cfbb0
bankmanx9x <nakamoorax9x@gmail.com>
2024-10-25 17:26:36 +0700

    complete test read pg database

--- pyscr/read_write_pg.py
+++ pyscr/read_write_pg.py
@@ -6,18 +6,23 @@ spark = SparkSession\
         .appName("chapter1PostgreSQL")\
         .config('spark.jars.packages', 'org.postgresql:postgresql:42.5.4')\
         .getOrCreate()
+
 sqlContext = SparkSession(spark)
 #Dont Show warning only error
 spark.sparkContext.setLogLevel("ERROR")
 
-postgredf = spark.read.format("jdbc") \
-    .option("url", "jdbc:postgresql://rds-db-test-yml-4.chh1ytozzee1.ap-southeast-1.rds.amazonaws.com:5432/postgres") \
-    .option("driver", "org.postgresql.Driver") \
-    .option("dbtable", "public.mpg") \
-    .option("user", "postgres") \
-    .option("password", "Ub44332148$")\
-    .load()
+postgredf = spark\
+            .read\
+            .format("jdbc") \
+            .option("url", "jdbc:postgresql://172.25.0.4:5432/postgres") \
+            .option("driver", "org.postgresql.Driver") \
+            .option("dbtable", "public.empl") \
+            .option("user", "postgres") \
+            .option("password", "postgres")\
+            .load()
 
 #postgredf.printSchema()
 
-postgredf.show()
+print(postgredf.show())
+
+spark.stop()
