0789974d2305c8c70f296ccfea510c252543c84e
bankmanx9x <nakamoorax9x@gmail.com>
2024-10-27 02:41:53 +0700

    read spark wh to pgsql

--- pyscr/pyspark_dbt.py
+++ pyscr/pyspark_dbt.py
@@ -1,9 +1,18 @@
 from pyspark.sql import SparkSession
+from dotenv import load_dotenv
+import os
 
 spark = SparkSession\
         .builder\
         .appName("Read dbt table")\
         .getOrCreate()
+
+load_dotenv()
+PGSQL_host = os.environ['PGSQL_host']
+PGSQL_port = os.environ['PGSQL_port']
+PGSQL_db = os.environ['PGSQL_db']
+PGSQL_user = os.environ['PGSQL_user']
+PGSQL_password = os.environ['PGSQL_password']
 
 df = spark\
         .read\
@@ -12,5 +21,18 @@ df = spark\
         .parquet("sparkwarehouse/dim_date")
 
 df.show()
+
+df\
+    .write\
+    .format("jdbc") \
+    .option("url", f"jdbc:postgresql://172.29.0.3:{PGSQL_port}/{PGSQL_db}") \
+    .option("driver","org.postgresql.Driver") \
+    .option("dbtable", "public.dim_date") \
+    .option("user", PGSQL_user) \
+    .option("password", PGSQL_password) \
+    .mode("overwrite") \
+    .save()
+
+print("Write Spark WH to PostgreSQL Table Successfully")
 
 spark.stop()
